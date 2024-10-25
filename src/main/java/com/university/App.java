20107bbae10289dcc8b1f28a9cb1d7d4cc4bbc17
TopoProgramer <brunitoalmiron86@gmail.com>
2024-10-25 11:09:07 -0300

    First part done

--- src/main/java/com/university/App.java
+++ src/main/java/com/university/App.java
@@ -3,6 +3,6 @@ package com.university;
 public class App {
     public static void main(String[] args) {
         CSVActions csvActions = new CSVActions();
-        csvActions.processCSV("C:\\Users\\Bruno\\OneDrive\\Escritorio\\Prog II\\Trabajo Practico Final\\src\\main\\resources\\input.csv", "C:\\Users\\Bruno\\OneDrive\\Escritorio\\Prog II\\Trabajo Practico Final\\src\\main\\resources\\solutions.csv");
+        csvActions.processCSV("src/main/resources/input.csv", "src/main/resources/solutions.csv");
     }
 }
